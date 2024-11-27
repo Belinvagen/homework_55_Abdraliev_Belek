b1f64d8455bf0a385207a3ad9df4ae01ee7659d9
Abdraliev Belek <abdraliev.belek04@gmail.com>
2024-11-28 01:13:32 +0600

    Добавлены представления и маршруты для задач

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
 
-# Create your views here.
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'todo/task_list.html', {'tasks': tasks})
+
+def task_add(request):
+    if request.method == 'POST':
+        description = request.POST.get('description')
+        status = request.POST.get('status')
+        due_date = request.POST.get('due_date')
+        Task.objects.create(description=description, status=status, due_date=due_date)
+        return redirect('task_list')
+    return render(request, 'todo/task_form.html')
