8edfa563f2204323cdf30696ced7d5a2cf39ead1
Abdraliev Belek <abdraliev.belek04@gmail.com>
2024-12-02 19:31:55 +0600

    Удалены файлы, которые не должны быть в репозитории

--- todo_app/apps.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def task_list(request):
-    return render(request, 'todo/task_list.html')
-
-def add_task(request):
-    return render(request, 'todo/add_task.html')
