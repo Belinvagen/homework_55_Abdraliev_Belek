a983ea60be0311d7ce38e113d6eb79878eff82ce
Abdraliev Belek <abdraliev.belek04@gmail.com>
2024-12-13 19:25:21 +0600

    Добавлены маршруты для редактирования и удаления задач

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('add/', views.task_add, name='task_add'),
-    path('task/<int:pk>/', views.task_detail, name='task_detail'),  # Новый маршрут
+    path('task/<int:pk>/', views.task_detail, name='task_detail'),
+    path('<int:pk>/edit/', views.edit_task, name='task_edit'),
+    path('<int:pk>/delete/', views.delete_task, name='task_delete'),
 ]
